aeae9662daafe8fd3ff4770d996b8b6e2c86e5d6
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-10-16 11:06:57 -0300

    feat: Adiciona classe que controla a api rest

---
+++ dac-exercicio1/src/main/java/com/dac/exercicio1/dacexercicio1/corsConfiguration/CorsConfigure.java
@@ -0,0 +1,16 @@
+package com.dac.exercicio1.dacexercicio1.corsConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
