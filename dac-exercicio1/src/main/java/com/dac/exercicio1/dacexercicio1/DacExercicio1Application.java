1da5c3635d4d424c085814b9e3f6a86933b8ccb6
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-10-16 08:27:03 -0300

    build: Cria projeto em spring boot

---
+++ dac-exercicio1/src/main/java/com/dac/exercicio1/dacexercicio1/DacExercicio1Application.java
@@ -0,0 +1,13 @@
+package com.dac.exercicio1.dacexercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacExercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacExercicio1Application.class, args);
+	}
+
+}
---
+++ dac-exercicio1/src/test/java/com/dac/exercicio1/dacexercicio1/DacExercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dac.exercicio1.dacexercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacExercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
